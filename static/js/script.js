dbc5fa6e952f43286d510cd456e6d7c4c8284b3f
jess-bennett <jess.bennett@posteo.net>
2021-01-31 14:24:29 +0000

    Add month header layout

--- static/js/script.js
+++ static/js/script.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
     $("#light-select").hide();
 
     $("#dark-select").click(function() {
-    $("body").removeClass("dark-theme");
-    $("body").addClass("light-theme");
+    $("body").addClass("dark-theme");
+    $("body").removeClass("light-theme");
     $("#dark-select").hide();
     $("#light-select").show();
 });
 
     $("#light-select").click(function() {
-    $("body").removeClass("light-theme");
-    $("body").addClass("dark-theme");
+    $("body").addClass("light-theme");
+    $("body").removeClass("dark-theme");
     $("#light-select").hide();
     $("#dark-select").show();
 });
