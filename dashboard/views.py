87decbb0b4eed0f0a4a38fc8cfb2bf784bccaf2f
jess-bennett <jess.bennett@posteo.net>
2021-02-07 13:38:00 +0000

    Add items to dashboard page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 def dashboard(request):
     """ A view to return the dashboard page """
-    return render(request, 'dashboard/dashboard.html')
+
+    items = Item.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'dashboard/dashboard.html', context)
