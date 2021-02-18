d000d187923804cb293d799359d44e0deb9a5e9a
jess-bennett <jess.bennett@posteo.net>
2021-02-18 19:12:39 +0000

    Import recipes model into plan view

--- plan/views.py
+++ plan/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Plan, Week
-from recipes.models import Month
+from recipes.models import Month, Recipe
 
 # Create your views here.
 
@@ -11,12 +11,14 @@ def plan(request):
     plans = Plan.objects.all()
     months = None
     weeks = None
+    recipes = Recipe.objects.all()
 
     if request.GET:
         if 'month' in request.GET:
             months = request.GET['month'].split(',')
             weeks = request.GET['week'].split(',')
             plans = plans.filter(month__name__in=months, week__name__in=weeks)
+            recipes = recipes.filter(month__name__in=months).order_by('name')
             months = Month.objects.filter(name__in=months)
             weeks = Week.objects.filter(name__in=weeks)
 
@@ -24,6 +26,7 @@ def plan(request):
         'plans': plans,
         'current_month': months,
         'current_week': weeks,
+        'recipes': recipes,
     }
 
     return render(request, 'plan/plan.html', context)
