260691eac3ae91b87c93d38ed80f55d762806ece
jess-bennett <jess.bennett@posteo.net>
2021-03-04 15:11:13 +0000

    Reload data to fix migration issue

--- membership/models.py
+++ membership/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from djstripe.models import Customer, Subscription
+from django.utils.functional import cached_property
+
+from djstripe.utils import subscriber_has_active_subscription
 
 
 class User(AbstractUser):
@@ -8,3 +11,14 @@ class User(AbstractUser):
         Customer, null=True, blank=True, on_delete=models.SET_NULL)
     subscription = models.ForeignKey(
         Subscription, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.username
+
+    def __unicode__(self):
+        return self.username
+
+    @cached_property
+    def has_active_subscription(self):
+        """Checks if a user has an active subscription."""
+        return subscriber_has_active_subscription(self)
