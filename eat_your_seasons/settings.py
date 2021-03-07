1caa88b469711d9ef82ca8796d548a78f00fd549
jess-bennett <jess.bennett@posteo.net>
2021-03-07 15:49:26 +0000

    Deployed to heroku

--- eat_your_seasons/settings.py
+++ eat_your_seasons/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'w!@m5x$ai#upei&t@df=$70(!fa7ud-5+0)pa6sadwa^4l$^w+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eat-your-seasons.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -115,12 +115,18 @@ AUTH_USER_MODEL = 'membership.User'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
