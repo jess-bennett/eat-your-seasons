3de270b58996c6d1a21ce45cf1e63ab7f25e2f64
jess-bennett <jess.bennett@posteo.net>
2021-03-10 13:47:48 +0000

    Style management pages

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -15,4 +15,5 @@ class ItemForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'management-box'
+            field.widget.attrs['style'] = 'text-transform:capitalize;'
